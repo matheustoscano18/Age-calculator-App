deb9596ee744843c6925559946f5b10e3151de41
Matheus <matheustoscano2012@gmail.com>
2024-10-29 18:58:51 -0300

    third commit

--- script.js
+++ script.js
@@ -1,35 +1,70 @@
 document.querySelector("button").addEventListener("click", ageCalculator);
 
 function ageCalculator() {
-  const birthYear = parseInt(document.getElementById("year").value, 10);
-  const birthMonth = parseInt(document.getElementById("month").value, 10);
-  const birthDay = parseInt(document.getElementById("day").value, 10);
+  const dayInput = document.getElementById("day");
+  const monthInput = document.getElementById("month");
+  const yearInput = document.getElementById("year");
+  const birthYear = parseInt(yearInput.value, 10);
+  const birthMonth = parseInt(monthInput.value, 10) - 1;
+  const birthDay = parseInt(dayInput.value, 10);
+  
+  const dayError = document.getElementById("day-error");
+  const monthError = document.getElementById("month-error");
+  const yearError = document.getElementById("year-error");
+  const dmy = document.querySelectorAll(".age-input p");
   const today = new Date();
 
-  if (!birthDay || !birthMonth || !birthYear) {
-    alert("Please enter a valid date");
-    return;
-  }
+  [dayInput, monthInput, yearInput].forEach(input => input.style.border = "1px solid var(--primaryPurple)");
+  [dayError, monthError, yearError].forEach(error => error.style.display = "none");
+
+  const requiredFields = [
+    { input: dayInput, error: dayError },
+    { input: monthInput, error: monthError },
+    { input: yearInput, error: yearError },
+  ];
+  requiredFields.forEach(({ input, error }) => {
+    if (!input.value) {
+      input.style.border = "1px solid var(--primaryRed)";
+      error.textContent = "This field is required";
+      error.style.display = "block";
+    }
+  });
 
   if (birthDay < 1 || birthDay > 31) {
-    alert("Please enter a valid day");
-    return;
+    dmy.style.color = "var(--primaryRed)";
+    dayInput.style.border = "1px solid var(--primaryRed)";
+    dayError.textContent = "Must be a valid day";
+    dayError.style.display = "block";
+    if (birthMonth < 1 || birthMonth > 12) {
+      monthInput.style.border = "1px solid var(--primaryRed)";
+      monthError.textContent = "Must be a valid month";
+      monthError.style.display = "block";
+      if (birthYear > today.getFullYear()) {
+      yearInput.style.border = "1px solid var(--primaryRed)";
+      yearError.textContent = "Must be a valid year";
+      yearError.style.display = "block";
+      return;
+      }
+    } 
   }
 
-  if (birthMonth < 1 || birthMonth > 12) {
-    alert("Please enter a valid month");
-    return;
-  }
-
-  const chosenDate = new Date(birthYear, birthMonth - 1, birthDay);
+  const chosenDate = new Date(birthYear, birthMonth, birthDay);
 
   if (
-    isNaN(chosenDate) ||
-    chosenDate.getDate() != birthDay ||
+    chosenDate.getDate() !== birthDay ||
+    chosenDate.getMonth() !== birthMonth ||
     chosenDate > today
-  ) {
-    alert("Invalid date. Verify your date and try again.");
-  }
+    ) {
+      [dayError, monthError, yearError].forEach(error => {
+        error.textContent = "Must be a valid date";
+        dayInput.style.border = "1px solid var(--primaryRed)";
+        monthInput.style.border = "1px solid var(--primaryRed)";
+        yearInput.style.border = "1px solid var(--primaryRed)";
+        error.style.display = "block";
+      })
+      [dayInput, monthInput, yearInput].forEach(input => input.style.border = "1px solid red");
+      return;
+    }
 
   let years = today.getFullYear() - birthYear;
   let months = today.getMonth() - birthMonth;
